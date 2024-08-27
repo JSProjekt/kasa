c107c3513ecc020e916e032cfd795080f9ba3c7e
Jordan S <168541998+JSProjekt@users.noreply.github.com>
2024-08-27 11:09:13 +0200

    update error same code in two place

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div>
     
-        <Banner imgBanner={'Imagesource1.png'} title='Chez vous, partout et ailleurs'/>
+        <Banner />
         <AppartementList />
     
     </div>
